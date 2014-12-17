b8cb4b2f9b91ead1a89f7bdfac5bed727dea14ae
Meyer Kizner <meyer@pre-alpha.com>
2014-12-17 03:07:02 -0500

    More efficient query

--- pennbook-get.js
+++ pennbook-get.js
@@ -59,23 +59,22 @@ define(['exports', 'aws-sdk'], function (exports, AWS) {
 
   exports.getEntity = function (eid, callback) {
     var params = {
-      KeyConditions: {
+      Key: {
         eid: {
           ComparisonOperator: 'EQ',
           AttributeValueList: [{ S: eid }]
         }
       },
-      TableName: 'entities',
-      Limit: 1
+      TableName: 'entities'
     };
-    dynamodb.query(params, function (err, data) {
-      if (err || !data.Items) {
+    dynamodb.getItem(params, function (err, data) {
+      if (err || !data.Item) {
         callback(null);
       } else {
         var result = {};
-        for (var attr in data.Items[0]) {
-          for (var type in data.Items[0][attr]) {
-            result[attr] = data.Items[0][attr][type];
+        for (var attr in data.Item) {
+          for (var type in data.Item[attr]) {
+            result[attr] = data.Item[attr][type];
           }
         }
         callback(result);
