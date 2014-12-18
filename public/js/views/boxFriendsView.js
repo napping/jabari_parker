0eceb06fca8fe6d5ce7f9d12f7b5506a91f50ae5
Brian Shi <brishi@seas.upenn.edu>
2014-12-17 23:49:30 -0500

    testing affiliation

--- public/js/views/boxFriendsView.js
+++ public/js/views/boxFriendsView.js
@@ -13,26 +13,39 @@ define([
 			template: _.template( boxFriendsTemplate ),
 
 			initialize: function (options) { 
-                this.friendDirectory = {};  // for making easier friend hrefs, maps email to eid
+                this.friendDirectory = {};  // for making easier friend hrefs, maps email to eid, used for network ppl as well
 
 			}, 
 
             events: { 
-                "click a": "showFriend",
+                "click .friend-link": "showFriend",
             },
 
 			render: function () { 
                 $(this.el).html( this.template() );
 
-                var view = this;
-                this.collection.each( function (friend) { 
-                    friend.set({ areFriends: true });
-                    view.friendDirectory[ friend.get("email") ] = friend;
-                    view.addFriendLink(friend);
-                });
+                if (this.friendsCollection && this.friendsCollection.length > 0) { 
+                    var view = this;
+                    this.friendsCollection.each( function (friend) { 
+                        friend.set({ areFriends: true });
+                        view.friendDirectory[ friend.get("email") ] = friend;
+                        view.addFriendLink(friend);
+                    });
 
-                if (this.collection.length == 0) { 
+                } else {
                     $(".friends-list > ul", this.el).append("<li><h5>No friends yet.</h5></li>");
+                }
+
+                if (this.networkCollection && this.networkCollection.length > 0) { 
+                    var view = this;
+                    this.networkCollection.each( function (friend) { 
+                        friend.set({ areFriends: true });
+                        view.friendDirectory[ friend.get("email") ] = friend;
+                        view.addNetworkLink(friend);
+                    });
+
+                } else {
+                    $(".network-list > ul", this.el).append("<li><h5>No one shares your affiliation.</h5></li>");
                 }
 
                 return this;
@@ -42,6 +55,12 @@ define([
                 var linkTemplate = _.template( "<li><a class=\"friend-link\" id=\"friend_" + friend.get("email") + "\"><%= firstName %> <%= lastName %><a></li>" );
                 $(".friends-list > ul", this.el).append( linkTemplate( friend.toJSON() ) );
             },
+
+            addNetworkLink: function (user) { 
+                var linkTemplate = _.template( "<li><a class=\"friend-link\" id=\"friend_" + user.get("email") + "\"><%= firstName %> <%= lastName %><a></li>" );
+                $(".network-list > ul", this.el).append( linkTemplate( user.toJSON() ) );
+            },
+
 
             showFriend: function (e) { 
                 vent.trigger( "showFriend", this.friendDirectory[(e.target.id).replace("friend_", "")]);
